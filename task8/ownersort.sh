6403ffe748b16395c436c067282190cb71da6de8
Перминов Сергей Олегович <S_Perminov@bz.ru>
2023-04-26 15:33:36 +0300

    ownersort is ready

--- task8/ownersort.sh
+++ task8/ownersort.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
 
+dir="$1"
+
+if ! [ -d $dir ]
+        then
+                echo "$dir does not exist"
+                exit 1
+fi
+
+if [ ! -z $dir ]
+        then
+                cd $dir
+fi
+
+shopt -s dotglob
+for file in *
+    do
+        if [ -f $file ]
+            then
+                user=`ls -l $file | awk {'print $3'}`
+
+                if [ ! -d $user ]
+                        then
+                                mkdir -p $user
+                fi
+
+                cp $file ./$user
+                chown $user ./$user/$file
+        fi
+    done
+
+exit 0
